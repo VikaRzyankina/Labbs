e5d94619617462213a872bfc3c79625948016ab0
VikaRzyankina <vika.rzyankina@mail.ru>
2023-09-18 22:37:34 +0400

    9lab

--- Laba9/9lab.py
+++ Laba9/9lab.py
@@ -3,45 +3,46 @@ import hashlib
 
 class Program:
     def __init__(self):
-        self.window = Tk(); self.window.title('Добрый день, пользователь!')
-        window=self.window
+        self.window = Tk()
+        self.window.title('Добрый день, пользователь!')
+        window = self.window
         window.eval('tk::PlaceWindow . center')
-        window.geometry('250x200')
+        window.geometry('300x200')
 
         Label(window, text='').pack()
         Label(window, text='').pack()
-        Button(window, font = 14, text="Регистрация", command=self.start).pack()
+        Button(window, font=14, text="Регистрация", command=self.start).pack()
         Label(window, text='').pack()
-        Button(window, font = 14, text="Вход", command=self.start_2).pack()
-        Button(window, font = 14 , text="Выход", command=window.destroy).pack(anchor=SE)
+        Button(window, font=14, text="Вход", command=self.start_2).pack()
+        Button(window, font=14, text="Выход", command=window.destroy).pack(anchor=SE)
 
         self.window.mainloop()
 
     def window_entry(self, description):
         Label(self.window, text=description).pack()
-        entry = Entry(self.window, font = 14, width = 20, justify='center')
+        entry = Entry(self.window, font=14, width=20, justify='center')
         entry.pack()
         return entry
 
     def wind_entry(self, description):
-        Label(self.wind, font = 14, text=description).pack()
-        entry = Entry(self.wind, font = 14, width = 20 , justify='center')
+        Label(self.wind, font=14, text=description).pack()
+        entry = Entry(self.wind, font=14, width=20, justify='center')
         entry.pack()
         return entry
 
     def start(self):
-        self.wind = Tk(); self.wind.title('Регистрация')
-        wind=self.wind
+        self.wind = Tk()
+        self.wind.title('Регистрация')
+        wind = self.wind
         wind.eval('tk::PlaceWindow . center')
-
 
         self.login_user = self.wind_entry('Придумайте логин')
         self.password_user = self.wind_entry('Придумайте пароль')
 
-        Button(wind, font = 14, text="Продолжить", command=self.write_txt).pack()
+        Button(wind, font=14, text="Продолжить", command=self.write_txt).pack()
 
     def start_2(self):
-        self.wind = Tk();
+        self.wind = Tk()
         self.wind.title('Вход')
         wind = self.wind
         wind.eval('tk::PlaceWindow . center')
@@ -49,7 +50,7 @@ class Program:
         self.login_user = self.wind_entry('Введите логин')
         self.password_user = self.wind_entry('Введите пароль')
 
-        Button(wind, font = 14, text="Продолжить", command=self.open_txt).pack()
+        Button(wind, font=14, text="Продолжить", command=self.open_txt).pack()
 
     def open_txt(self):
         NotFound = True
@@ -57,14 +58,13 @@ class Program:
             read = f.readlines()
             password_sha = hashlib.sha1(str.encode(self.password_user.get())).hexdigest()
             for i in range(0, len(read), 2):
-                if self.login_user.get() == read[i].rstrip('\n') and password_sha == read[i+1].rstrip('\n'):
-                    Label(self.wind, font = 14, text='Вы успешно авторизовались').pack()
-                    NotFound=False
+                if self.login_user.get() == read[i].rstrip('\n') and password_sha == read[i + 1].rstrip('\n'):
+                    Program.text_window('Вы успешно авторизовались', 'Авторизация')
+                    self.wind.destroy()
+                    NotFound = False
                     break
         if NotFound == True:
             Program.text_window('Не удалось авторизоваться. Не подходящие логин или пароль.')
-        else:
-            Button(self.wind, font=14, text="Выход", command=self.wind.destroy).pack(anchor=SE)
 
     def write_txt(self):
         with open('File.txt', 'r') as f:
@@ -77,16 +77,19 @@ class Program:
                 Program.text_window('Вы оставили поле пустым.')
                 return
         with open('File.txt', 'a') as f:
-            Label(self.wind, font = 14, text='Вы успешно зарегистрировались.').pack()
-            f.write('\n' + self.login_user.get() + '\n' + hashlib.sha1(str.encode(self.password_user.get())).hexdigest())
+            Program.text_window('Вы успешно зарегистрировались.', 'Регистрация')
+            f.write(
+                '\n' + self.login_user.get() + '\n' + hashlib.sha1(str.encode(self.password_user.get())).hexdigest())
             f.close()
-        Button(self.wind, font=14, text="Выход", command=self.wind.destroy).pack(anchor=SE)
+            self.wind.destroy()
 
-    def text_window(message):  # функция для вывода текста об ошибке
+    def text_window(message, title = None):  # функция для вывода текста об ошибке
         window = Tk()
-        window.title('Ошибка')
-        Label(window, font = 14, text=message).pack()
+        if title != None:
+            window.title(title)
+        else:
+            window.title('Ошибка')
+        Label(window, font=14, text=message).pack()
         window.eval('tk::PlaceWindow . center')
 
 program = Program()
-
