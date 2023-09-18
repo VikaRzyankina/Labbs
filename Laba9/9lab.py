e8d45d04fd6858c19c1cc55b862781cfac7352fa
VikaRzyankina <vika.rzyankina@mail.ru>
2023-09-18 22:22:43 +0400

    9lab

---
+++ Laba9/9lab.py
@@ -0,0 +1,92 @@
+from tkinter import *
+import hashlib
+
+class Program:
+    def __init__(self):
+        self.window = Tk(); self.window.title('Добрый день, пользователь!')
+        window=self.window
+        window.eval('tk::PlaceWindow . center')
+        window.geometry('250x200')
+
+        Label(window, text='').pack()
+        Label(window, text='').pack()
+        Button(window, font = 14, text="Регистрация", command=self.start).pack()
+        Label(window, text='').pack()
+        Button(window, font = 14, text="Вход", command=self.start_2).pack()
+        Button(window, font = 14 , text="Выход", command=window.destroy).pack(anchor=SE)
+
+        self.window.mainloop()
+
+    def window_entry(self, description):
+        Label(self.window, text=description).pack()
+        entry = Entry(self.window, font = 14, width = 20, justify='center')
+        entry.pack()
+        return entry
+
+    def wind_entry(self, description):
+        Label(self.wind, font = 14, text=description).pack()
+        entry = Entry(self.wind, font = 14, width = 20 , justify='center')
+        entry.pack()
+        return entry
+
+    def start(self):
+        self.wind = Tk(); self.wind.title('Регистрация')
+        wind=self.wind
+        wind.eval('tk::PlaceWindow . center')
+
+
+        self.login_user = self.wind_entry('Придумайте логин')
+        self.password_user = self.wind_entry('Придумайте пароль')
+
+        Button(wind, font = 14, text="Продолжить", command=self.write_txt).pack()
+
+    def start_2(self):
+        self.wind = Tk();
+        self.wind.title('Вход')
+        wind = self.wind
+        wind.eval('tk::PlaceWindow . center')
+
+        self.login_user = self.wind_entry('Введите логин')
+        self.password_user = self.wind_entry('Введите пароль')
+
+        Button(wind, font = 14, text="Продолжить", command=self.open_txt).pack()
+
+    def open_txt(self):
+        NotFound = True
+        with open('File.txt', 'r') as f:
+            read = f.readlines()
+            password_sha = hashlib.sha1(str.encode(self.password_user.get())).hexdigest()
+            for i in range(0, len(read), 2):
+                if self.login_user.get() == read[i].rstrip('\n') and password_sha == read[i+1].rstrip('\n'):
+                    Label(self.wind, font = 14, text='Вы успешно авторизовались').pack()
+                    NotFound=False
+                    break
+        if NotFound == True:
+            Program.text_window('Не удалось авторизоваться. Не подходящие логин или пароль.')
+        else:
+            Button(self.wind, font=14, text="Выход", command=self.wind.destroy).pack(anchor=SE)
+
+    def write_txt(self):
+        with open('File.txt', 'r') as f:
+            read = f.readlines()
+            for i in range(0, len(read), 2):
+                if self.login_user.get() == read[i].rstrip('\n'):
+                    Program.text_window('Выбранный вами логин уже используется.')
+                    return
+            if self.login_user.get() == '' or self.password_user.get() == '':
+                Program.text_window('Вы оставили поле пустым.')
+                return
+        with open('File.txt', 'a') as f:
+            Label(self.wind, font = 14, text='Вы успешно зарегистрировались.').pack()
+            f.write('\n' + self.login_user.get() + '\n' + hashlib.sha1(str.encode(self.password_user.get())).hexdigest())
+            f.close()
+        Button(self.wind, font=14, text="Выход", command=self.wind.destroy).pack(anchor=SE)
+
+    def text_window(message):  # функция для вывода текста об ошибке
+        window = Tk()
+        window.title('Ошибка')
+        Label(window, font = 14, text=message).pack()
+        window.eval('tk::PlaceWindow . center')
+
+program = Program()
+
--- laba8/lab8.py
+++ laba8/lab8.py
@@ -8,7 +8,7 @@
 + Сумма элементов столбца изначального массива проверяемого элемента должна быть не отрицательной.
 
 '''
-
+from tkinter import *
 import tkinter as tk
 import random
 import copy
@@ -16,29 +16,31 @@ import copy
 class Program:
     def __init__(self,main):
         self.main = main
-        tk.Label(main, text="Дан двумерный массив. Сформировать все возможные варианты данного массива путем"
+        tk.Label(main, font = 14, text="Дан двумерный массив. Сформировать все возможные варианты данного массива путем"
                                    "\nзамены положительных элементов в четных столбцах на отрицательные элементы, равные по модули."
                                    "\nПроверяемый элемент массива не должен нацело делиться на 3."
                                    "\nСумма элементов столбца изначального массива проверяемого элемента должна быть не отрицательной.").pack()
 
 
-        tk.Label(main, text="Введите ширину массива: ").pack()
-        self.entry1 = tk.Entry(main, width=30, justify='center')
+        tk.Label(main, font = 14, text="Введите ширину массива: ").pack()
+        self.entry1 = tk.Entry(main, font = 14, width=30, justify='center')
         self.entry1.pack()
 
-        tk.Label(main, text="Введите высоту массива: ").pack()
-        self.entry2 = tk.Entry(main, width=30, justify='center')
+        tk.Label(main, font = 14, text="Введите высоту массива: ").pack()
+        self.entry2 = tk.Entry(main, font = 14, width=30, justify='center')
         self.entry2.pack()
 
-        tk.Label(main, text='Выберите желаемый тип перебора массива').pack()
+        tk.Label(main, font = 14, text='Выберите желаемый тип перебора массива').pack()
         self.entry3 = tk.StringVar()
         self.entry3.set('Итеративный')
-        tk.OptionMenu(main, self.entry3, *['Итеративный', 'Рекурсивный']).pack()
+        opt = tk.OptionMenu(main, self.entry3,  *['Итеративный', 'Рекурсивный'])
+        opt.config( font=('Helvetica', 14))
+        opt.pack()
 
-        self.button_main = tk.Button(main,text="Начать", command=self.check_input)
+        self.button_main = tk.Button(main, font = 14,text="Начать", command=self.check_input)
         self.button_main.pack()
 
-        self.button_exit = tk.Button(main, text="Выход", command=window.destroy)
+        self.button_exit = tk.Button(main, font = 14, text="Выход", command=window.destroy)
         self.button_exit.pack(anchor="se")
         self.main.mainloop()
 
@@ -58,9 +60,9 @@ class Program:
         array = [[random.randint(-10, 11) for i in range(self.row)] for j in range(self.column)]
         summ = [0] * self.row
         root = tk.Tk()
-        tk.Label(root, text='Начальный массив:').pack()
-        start_array = tk.Text(root, height=self.column, width=self.row*5, wrap=tk.NONE)
-        start_array.tag_configure("center", justify=tk.CENTER)
+        tk.Label(root, font = 14, text='Начальный массив:').pack()
+        start_array = tk.Text(root, height=self.column*2, width=self.row*5, wrap=tk.NONE)
+        start_array.tag_configure("center", font = 14, justify=tk.CENTER)
         start_array.insert(tk.END, array_string(array))
         start_array.tag_add('center', '1.0', 'end')
         start_array.pack()
@@ -68,16 +70,16 @@ class Program:
             for j in range(self.column):
                 if i % 2 == 1:
                     summ[i] += array[j][i]
-        list_arrays = tk.Text(root, height=15, width=self.row*5, wrap=tk.NONE)
-        list_arrays.tag_configure("center", justify=tk.CENTER)
+        list_arrays = tk.Text(root, height=self.column*5, width=self.row*5, wrap=tk.NONE)
+        list_arrays.tag_configure("center", font = 14, justify=tk.CENTER)
         count = self.handler.process(array, summ, root, list_arrays) - 1
         list_arrays.tag_add('center', '1.0', 'end')
         list_arrays.pack()
         root.title("Ответ")
-        tk.Label(root,width=90, text=f'Общее количество вариантов: {count}').pack()
+        tk.Label(root,width=90, font = 14, text=f'Общее количество вариантов: {count}').pack()
         if count == 0:
-            tk.Label(root, width=90, text='Проверяемый массив не подошел под условия.').pack()
-        self.button_exit = tk.Button(root, text="Выход", command=root.destroy)
+            tk.Label(root, width=90, font = 14, text='Проверяемый массив не подошел под условия.').pack()
+        self.button_exit = tk.Button(root, font = 14, text="Выход", command=root.destroy)
         self.button_exit.pack(anchor="se")
 
 class Handler:
@@ -87,7 +89,7 @@ class Handler:
 
 class IterativeHandler(Handler):
     def process(self, array, summ, root, list_arrays):
-        tk.Label(root, width=90, text='Итеративный перебор возможных вариантов.').pack()
+        tk.Label(root, font = 14, width=90, text='Итеративный перебор возможных вариантов.').pack()
         count = 0
         stack = [(copy.deepcopy(array), 0, 0)]
 
@@ -111,7 +113,7 @@ class IterativeHandler(Handler):
 
 class RecursiveHandler(Handler):
     def process(self, array, summ, root, list_arrays):
-        tk.Label(root, text='Рекурсивный перебор возможных вариантов.').pack()
+        tk.Label(root, font = 14, text='Рекурсивный перебор возможных вариантов.').pack()
         count = [0]
         self._process(array, 0, 0, count, summ, root, list_arrays)
         return count[0]
@@ -132,10 +134,11 @@ class RecursiveHandler(Handler):
             new_array[row][column] = -new_array[row][column]
             self._process(new_array, row, column + 1, count, summ, root, list_arrays)
 
-def text_window(message):
+def text_window(message): #функция для вывода текста об ошибке
   window = tk.Tk()
   window.title('Ошибка')
-  tk.Label(window, text=message).pack()
+  window.eval('tk::PlaceWindow . center')
+  tk.Label(window, font = 14, text=message).pack()
 
 
 def accept_number(value):  # Проверка символа на цифру
@@ -149,7 +152,7 @@ def accept_number(value):  # Проверка символа на цифру
         text_window("Введенное значение не является числом.")
 
 def array_string(array):
-    array_str='' # Вывод массива
+    array_str='' # Вывод массива визуально
     for row in array:
         for elem in row:
             array_str += ('{:4} '.format(elem))
