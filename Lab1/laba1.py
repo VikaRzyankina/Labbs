e3fc994ae2af1f112babd05520877d12ac0982bb
Виктория Рзянкина <124519094+VikaRzyankina@users.noreply.github.com>
2024-01-19 09:58:17 +0400

    Update laba1.py

--- Lab1/laba1.py
+++ Lab1/laba1.py
@@ -1,17 +1,18 @@
-file = input('Введите название файла с расширением:')
-filename = file
 count = 0
 summ = 0
-while filename.find('.txt') == -1:
-    filename = input('Вы не ввели расширение .txt. Введите название файла с расширением:')
-    with open(filename, 'r') as f:
-        buffer = f.read().split(' ')
-        print(buffer)
-        for i in range(len(buffer)):
-            
-            if str(buffer[i]) % 2 == 0 and len(str(buffer)) == 5 and str(buffer[0]) == '7':
-                count += 1
-                summ += buffer[i]
-            else:
-                continue
-            print(count, summ)
+while True:
+    file = input('Введите название файла с расширением:')
+    filename = file
+    try:
+        with open(filename, 'r') as f:
+            buffer = f.read().split(' ')
+            for i in buffer:
+                if int(i) % 2 == 0 and len(str(i)) == 5 and str(i)[0] == '7':
+                    count += 1
+                    summ += int(i)
+                else:
+                    continue
+        print('Количество:',count,',Cумма:', summ)
+        break
+    except FileNotFoundError:
+        continue
